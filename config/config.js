f222447e0b03df3171b692203f962aee1b9f2767
Hwanpago0107 <55492770+Hwanpago0107@users.noreply.github.com>
2021-09-22 13:27:36 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "1234",
+    "password": "****",
     "database": "COWORK",
     "host": "127.0.0.1",
     "dialect": "mysql"
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB,
+    "host": process.env.HOST,
     "dialect": "mysql"
   }
 }
